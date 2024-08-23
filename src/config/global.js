06f0d32dcae4b22ecb757ac58f674c7f1f270479
emilydev <98632409+emilydev@users.noreply.github.com>
2024-08-22 23:04:13 -0500

    Se carga glosario y material complementario y referencias.

--- src/config/global.js
+++ src/config/global.js
@@ -141,32 +141,138 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Conservación de los alimentos',
+      referencia:
+        'Organización Panamericana de la Salud. (2014). Manual para manipuladores de alimentos. Organización Panamericana de la Salud.',
+      tipo: 'Documento',
+      descarga:
+        'https://www3.paho.org/hq/dmdocuments/manual-manipuladores-alimentos-2014.pdf',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Color del alimento',
+      referencia:
+        'Mundo Agroindustrial (2016). Control de calidad de alimentos - Garantía para el consumidor. [Archivo de video] Youtube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=OMa_7rCqLlc',
+    },
+    {
+      tema: 'Prácticas sanitarias y de salud',
+      referencia:
+        'Javier Vega (2021). Normas de higiene para el personal manipulador de alimentos. [Archivo de video] Youtube.',
+      tipo: 'Video',
+      link: 'https://youtu.be/aoOh7wqNpDM?feature=shared',
+    },
+    {
+      tema: 'Precauciones en la manipulación',
+      referencia:
+        'Alimentos Cloud (s.f.). Manipulación segura de alimentos. Curso de Manipulador de Alimentos. [Podcast]. YouTube.',
+      tipo: 'Podcast',
+      descarga: 'https://music.youtube.com/watch?v=6FFMBqzVw30&feature=shared',
+    },
+    {
+      tema: 'Preparación de alimentos',
+      referencia:
+        'SENA. (2012). Higiene y Manipulación de Alimentos Cap 04.mp4 [Archivo de video] Youtube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=aeqWCTYT-qk&ab_channel=SENA',
+    },
+    {
+      tema: 'Condiciones de almacenamiento',
+      referencia:
+        'Ministerio de Salud y Protección Social. (s.f.). Guía de inocuidad de alimentos en establecimientos de almacenamiento.',
+      tipo: 'Guía',
+      descarga:
+        'https://www.minsalud.gov.co/sites/rid/Lists/BibliotecaDigital/RIDE/VS/PP/SNA/Guia-inocuidad-alimentos-establecimientos-almacenamiento.pdf',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Conservación',
+      significado:
+        'conjunto de métodos y prácticas utilizadas para mantener la calidad y seguridad de los alimentos.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Contaminación cruzada',
+      significado:
+        'transferencia de microorganismos perjudiciales de un alimento a otro, generalmente de crudo a cocido.',
+    },
+    {
+      termino: 'Deshidratación',
+      significado:
+        'método de conservación que elimina el agua de los alimentos para inhibir el crecimiento de microorganismos.',
+    },
+    {
+      termino: 'Fermentación',
+      significado:
+        'proceso químico en el cual microorganismos convierten azúcares en alcohol o ácidos, preservando ciertos alimentos.',
+    },
+    {
+      termino: 'Higiene',
+      significado:
+        'prácticas de limpieza y desinfección para prevenir la contaminación de alimentos.',
+    },
+    {
+      termino: 'Liofilización',
+      significado:
+        'técnica de deshidratación que congela los alimentos y luego reduce la presión para eliminar el agua por sublimación.',
+    },
+    {
+      termino: 'Organolépticas',
+      significado:
+        'características de los alimentos que afectan los sentidos, como sabor, color, olor y textura.',
+    },
+    {
+      termino: 'Pasteurización',
+      significado:
+        'proceso de calentamiento de alimentos a una temperatura específica para eliminar patógenos.',
+    },
+    {
+      termino: 'Preenfriamiento',
+      significado:
+        'técnica de enfriamiento rápido de frutas y verduras después de la cosecha para prolongar su vida útil.',
+    },
+    {
+      termino: 'Rango de temperaturas de peligro',
+      significado:
+        'intervalo de temperatura en el cual los microorganismos pueden proliferar rápidamente (5 ºC A 65 ºC).',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Bravo, F. (2004). Manejo higiénico de los alimentos. México. Limusa.',
+    },
+    {
+      referencia:
+        'Clayton, K. (s.f.).  Métodos para la conservación de alimentos. Revista "Emprendimientos alimentarios". University Purdue.',
+      link: 'https://www.extension.purdue.edu/extmedia/FS/FS-15-S-W.pdf',
+    },
+    {
+      referencia:
+        'Desrosier, N. (1991). The technology of food preservation. New York: The Avi Publishing Company, Inc.',
+    },
+    {
+      referencia:
+        'Díaz Torres, R. (2009). Conservación de los alimentos: ( ed.). Editorial Félix Varela.',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/lc/senavirtual/titulos/71247',
+    },
+    {
+      referencia:
+        'Editorial Eidec. (2020). La inocuidad de los alimentos y su aporte a la seguridad alimentaria.',
+      link:
+        'https://www.editorialeidec.com/wp-content/uploads/2020/11/Libro-la-inocuidad-de-alimentos-y-su-aporte-a-la-seguridad-alimentaria.pdf',
+    },
+    {
+      referencia:
+        'FAO. (2013). Almacenamiento no refrigerado o refrigerado de la carne fresca y los subproductos comestibles.',
+      link: 'http://www.fao.org/docrep/004/t0566s/t0566s12.htm',
+    },
+    {
+      referencia:
+        'Organización Mundial de la Salud (OMS). (2002). Estrategia global de la OMS para la inocuidad de los alimentos: alimentos más sanos para una salud mejor. Ginebra: Departamento de Inocuidad de los Alimentos.',
+      link:
+        'https://cdn.who.int/media/docs/default-source/food-safety/campaign-guide-es.pdf',
     },
   ],
   creditos: [
@@ -189,8 +295,13 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
+          nombre: 'Ángela Viviana Páez Perilla',
+          cargo: 'Experta temática',
+          centro: 'Centro Agroindustrial - Regional Quindío',
+        },
+        {
+          nombre: 'Paola Alexandra Moya',
+          cargo: 'Evaluadora instruccional',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
       ],
@@ -199,12 +310,12 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Andrés Felipe Herrera Roldán',
           cargo: 'Diseñador de contenidos',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Edgar Mauricio Cortes Garcia',
           cargo: 'Desarrollador <i>full stack</i>',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -15,10 +15,10 @@ export default {
   components: { Actividad },
   data: () => ({
     cuestionario: {
-      tema: 'Manejo de los residuos sólidos en la producción avícola.',
+      tema: 'Conservación y manipulación segura de alimentos.',
       titulo: 'Cuestionario',
       introduccion:
-        'Lea cada enunciado y luego seleccione la respuesta correcta según corresponda.',
+        'Lea la pregunta de cada ítem y seleccione la respuesta correcta.',
       barajarPreguntas: false,
       preguntas: [
         {
@@ -73,27 +73,46 @@ export default {
         },
         {
           id: 3,
-          texto: 'Texto de la tercera pregunta 3',
+          texto:
+            '¿Cuál de los siguientes rangos de temperatura se considera peligroso para el crecimiento de microorganismos en los alimentos?',
           imagen: require('@/assets/actividad/q3.png'),
           opciones: [
-            { id: 'a', texto: 'Opción A', esCorrecta: false },
-            { id: 'b', texto: 'Opción B', esCorrecta: false },
-            { id: 'c', texto: 'Opción C', esCorrecta: false },
-            { id: 'd', texto: 'Opción D', esCorrecta: true },
+            { id: 'a', texto: '0 ºC - 5 ºC.', esCorrecta: false },
+            { id: 'b', texto: '5 ºC - 65 ºC.', esCorrecta: true },
+            { id: 'c', texto: '65 ºC - 100 ºC.', esCorrecta: false },
+            { id: 'd', texto: '-18 ºC - 0 ºC.', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Felicitaciones, has acertado.',
-          mensaje_incorrecto: 'Respuesta incorrecta. ¡Inténtalo de nuevo!',
+          mensaje_incorrecto:
+            'La respuesta es incorrecta. Te sugerimos revisar nuevamente el componente formativo.',
         },
         {
           id: 4,
-          texto: 'Texto de la cuarta pregunta 4',
+          texto:
+            '¿Cuál es una práctica recomendada para evitar la contaminación cruzada en la cocina?',
           imagen: require('@/assets/actividad/q4.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: 'Opción A', esCorrecta: true },
-            { id: 'b', texto: 'Opción B', esCorrecta: false },
-            { id: 'c', texto: 'Opción C', esCorrecta: false },
-            { id: 'd', texto: 'Opción D', esCorrecta: false },
+            {
+              id: 'a',
+              texto: 'Cocinar los alimentos parcialmente.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Almacenar alimentos crudos y cocidos juntos.',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'Lavar y desinfectar los utensilios de cocina.',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Consumir alimentos inmediatamente después de cocinarlos.',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Felicitaciones, has acertado.',
           mensaje_incorrecto:
@@ -101,13 +120,14 @@ export default {
         },
         {
           id: 5,
-          texto: 'Texto de la quinta pregunta 5',
+          texto:
+            '¿Qué método de conservación utiliza la reducción de agua para inhibir el crecimiento microbiano?',
           imagen: require('@/assets/actividad/q1.png'),
           opciones: [
-            { id: 'a', texto: 'Opción A', esCorrecta: false },
-            { id: 'b', texto: 'Opción B', esCorrecta: true },
-            { id: 'c', texto: 'Opción C', esCorrecta: false },
-            { id: 'd', texto: 'Opción D', esCorrecta: false },
+            { id: 'a', texto: 'Refrigeración.', esCorrecta: false },
+            { id: 'b', texto: 'Congelación.', esCorrecta: false },
+            { id: 'c', texto: 'Deshidratación.', esCorrecta: true },
+            { id: 'd', texto: 'Pasteurización.', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Felicitaciones, has acertado.',
           mensaje_incorrecto:
@@ -115,13 +135,14 @@ export default {
         },
         {
           id: 6,
-          texto: 'Texto de la quinta pregunta 6',
+          texto:
+            '¿Cuál de los siguientes no es un sabor fundamental reconocido en la naturaleza?',
           imagen: require('@/assets/actividad/q2.png'),
           opciones: [
-            { id: 'a', texto: 'Opción A', esCorrecta: false },
-            { id: 'b', texto: 'Opción B', esCorrecta: true },
-            { id: 'c', texto: 'Opción C', esCorrecta: false },
-            { id: 'd', texto: 'Opción D', esCorrecta: false },
+            { id: 'a', texto: 'Agrio.', esCorrecta: false },
+            { id: 'b', texto: 'Dulce.', esCorrecta: false },
+            { id: 'c', texto: 'Umami.', esCorrecta: true },
+            { id: 'd', texto: 'Salado.', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Felicitaciones, has acertado.',
           mensaje_incorrecto:
@@ -129,13 +150,14 @@ export default {
         },
         {
           id: 7,
-          texto: 'Texto de la quinta pregunta 7',
+          texto:
+            '¿Qué factor biológico puede causar la alteración de un alimento?',
           imagen: require('@/assets/actividad/q3.png'),
           opciones: [
-            { id: 'a', texto: 'Opción A', esCorrecta: false },
-            { id: 'b', texto: 'Opción B', esCorrecta: true },
-            { id: 'c', texto: 'Opción C', esCorrecta: false },
-            { id: 'd', texto: 'Opción D', esCorrecta: false },
+            { id: 'a', texto: 'Luz.', esCorrecta: false },
+            { id: 'b', texto: 'Humedad.', esCorrecta: false },
+            { id: 'c', texto: 'Bacterias.', esCorrecta: true },
+            { id: 'd', texto: 'Oxígeno.', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Felicitaciones, has acertado.',
           mensaje_incorrecto:
@@ -143,13 +165,14 @@ export default {
         },
         {
           id: 8,
-          texto: 'Texto de la quinta pregunta 8',
+          texto:
+            '¿Cuál es la distancia mínima recomendada entre los productos y las paredes de las cámaras de refrigeración?',
           imagen: require('@/assets/actividad/q4.png'),
           opciones: [
-            { id: 'a', texto: 'Opción A', esCorrecta: false },
-            { id: 'b', texto: 'Opción B', esCorrecta: true },
-            { id: 'c', texto: 'Opción C', esCorrecta: false },
-            { id: 'd', texto: 'Opción D', esCorrecta: false },
+            { id: 'a', texto: '10 cm.', esCorrecta: false },
+            { id: 'b', texto: '15 cm.', esCorrecta: true },
+            { id: 'c', texto: '20 cm.', esCorrecta: false },
+            { id: 'd', texto: '25 cm.', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Felicitaciones, has acertado.',
           mensaje_incorrecto:
@@ -157,13 +180,26 @@ export default {
         },
         {
           id: 9,
-          texto: 'Texto de la quinta pregunta 9',
+          texto:
+            '¿Cuál de las siguientes acciones es importante realizar antes de manipular alimentos?',
           imagen: require('@/assets/actividad/q1.png'),
           opciones: [
-            { id: 'a', texto: 'Opción A', esCorrecta: false },
-            { id: 'b', texto: 'Opción B', esCorrecta: true },
-            { id: 'c', texto: 'Opción C', esCorrecta: false },
-            { id: 'd', texto: 'Opción D', esCorrecta: false },
+            {
+              id: 'a',
+              texto: 'Calentar los utensilios de cocina.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Lavar las manos con agua y jabón.',
+              esCorrecta: true,
+            },
+            { id: 'c', texto: 'Desinfectar los alimentos.', esCorrecta: false },
+            {
+              id: 'd',
+              texto: 'Probar el sabor de los ingredientes.',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Felicitaciones, has acertado.',
           mensaje_incorrecto:
@@ -171,13 +207,14 @@ export default {
         },
         {
           id: 10,
-          texto: 'Texto de la quinta pregunta 10',
+          texto:
+            '¿Qué temperatura mínima se recomienda para cocinar carnes para asegurar la eliminación de microorganismos patógenos? ',
           imagen: require('@/assets/actividad/q2.png'),
           opciones: [
-            { id: 'a', texto: 'Opción A', esCorrecta: false },
-            { id: 'b', texto: 'Opción B', esCorrecta: true },
-            { id: 'c', texto: 'Opción C', esCorrecta: false },
-            { id: 'd', texto: 'Opción D', esCorrecta: false },
+            { id: 'a', texto: '55 ºC.', esCorrecta: false },
+            { id: 'b', texto: '60 ºC.', esCorrecta: false },
+            { id: 'c', texto: '65 ºC.', esCorrecta: true },
+            { id: 'd', texto: '70 ºC.', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Felicitaciones, has acertado.',
           mensaje_incorrecto:
@@ -185,9 +222,9 @@ export default {
         },
       ],
       mensaje_final_aprobado:
-        '¡Has completado el cuestionario exitosamente! Felicidades.',
+        '¡Excelente! Te felicito, has superado la actividad.',
       mensaje_final_reprobado:
-        'No has alcanzado la puntuación mínima para aprobar. Te animamos a intentarlo de nuevo.',
+        'Te recomendamos volver a revisar el componente formativo e intentar nuevamente la actividad didáctica.',
     },
   }),
   computed: {},
